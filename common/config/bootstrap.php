6ebce078b2eae6d56891b2387cb124a08c732ec4
Gleb <shukhleev.talan@gmail.com>
2025-10-29 22:53:40 +0500

    added .env.dist and etc.

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,13 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+
+if (!function_exists('env')) {
+    function env(string $key, $default = null) {
+        $value = $_ENV[$key] ?? getenv($key);
+        if ($value === false) {
+            return $default;
+        }
+        return $value;
+    }
+}
